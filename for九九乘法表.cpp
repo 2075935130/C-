56ec99cfbfd1209e4dfe780eb8fe39430b976677
旅行者 <135569677+2075935130@users.noreply.github.com>
2023-06-07 15:15:15 +0800

    Add files via upload

---
+++ for九九乘法表.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+int x,y;
+int temp;
+  for(x=1;x<=9;x++)
+  {
+  	for(y=1;y<=x;y++)
+	  {
+	  	printf("%d*%d=%d",x,y,x*y);
+	  	
+		   } 	
+  	
+  	printf("\n");
+  	
+  }
+	
+	
+	
+	
+	
+	return 0;
+ } 
