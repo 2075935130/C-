d7fe7298c2ea237f2c91cc23b41c055e26f8250a
旅行者 <135569677+2075935130@users.noreply.github.com>
2023-06-08 09:03:48 +0800

    Add files via upload

---
+++ 用数组求的年月日显示多少天.cpp
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+ int n[12]={31,28,31,30,31,30,31,31,30,31,30,31};
+ int x,y,z;
+ int i,sum;
+ printf("请输入年月日：");
+ scanf("%d%d%d",&z,&y,&z);
+ for(i=0;i<y-1;i++)
+ {
+ 	sum=sum+n[i];
+ 	
+ }
+ sum=sum+z;
+ if((y>2)&&((x%4==0&&x%100!=0)||(x%400==0)))
+ {
+ 	sum=sum+1;
+ }
+ printf("sum=%d\n",sum);
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+return 0;
+}
