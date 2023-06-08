cfecc90cbe916bfbb942047dabf23878f9acb468
旅行者 <135569677+2075935130@users.noreply.github.com>
2023-06-08 09:10:08 +0800

    Add files via upload

---
+++ 输入1到7判断星期几.cpp
@@ -0,0 +1,36 @@
+#include<stdio.h>
+int main()
+{ int i=0;
+  scanf("%d",&i);
+  switch(i)
+  {
+  case 1:
+  	printf("星期1\n");
+  	break;
+  	case 2:
+  	printf("星期2\n");
+  	break;
+  	case 3:
+  	printf("星期\3n");
+  	break;
+  	case 4:
+  	printf("星期4\n");
+  	break;
+  	case 5:
+  	printf("星期5\n");
+  	break;
+  	case 6:
+  	printf("星期6\n");
+  	break;
+  	case 7:
+  	printf("星期7\n");
+  	break;
+  
+}
+	
+	
+	
+	
+	
+	return 0;
+}
