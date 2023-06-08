af3dad48958a1823ae2b6409e65f90729a58fca2
旅行者 <135569677+2075935130@users.noreply.github.com>
2023-06-08 08:55:04 +0800

    Add files via upload

---
+++ 数组求和.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+	int a[5]={3,5,8,4,6};
+	int *p=&a[0];
+	; 
+	int max,i;
+	
+	   for(i=0;i<5;i++)
+	   {
+		
+		max+=*(p+i);
+     	}
+	     printf("数组的和：%d\n",max);
+	
+	
+	
+	
+	
+	
+	return 0;
+ } 
